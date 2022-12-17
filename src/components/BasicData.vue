6012dc6f83f7d14c238f0266a4342ef67b13332b
log101 <frknerdm@mail.com>
2022-12-17 16:27:28 +0300

    Add hourly weather forecasts

--- src/components/BasicData.vue
+++ src/components/BasicData.vue
@@ -2,14 +2,39 @@
     <div v-if="currentData" class="w-100 p-3 text-light">
         <h1 class="display-3 mb-0">{{ currentTemperature }}°C</h1>
         <span class="text-light opacity-75">{{ currentData.weather[0].description }}</span>
-        
-    </div>
-    <div style="background-color: black;" class="mx-auto">
 
-        <!-- <h6 class="display-7 m-3">{{ getDate(currentData.dt) }}</h6> -->
-        <h6 class="display-7 m-3">asşdmas</h6>
-        <h6 class="display-6 b-3">24°C</h6>
-        <img src="@/assets/sun.png" alt="sun" class="mx-auto" style="height: 100px;">
+    </div>
+
+    <!-- <h6 class="display-7 m-3">{{ getDate(currentData.dt) }}</h6> -->
+    <div class="h-100 w-100 p-3 d-flex flex-row">
+        <div class="card bg-dark m-2" style="text-align: center;">
+            <div class="card-body">
+                <span class="text-white-50">24 Saat   </span><span>13:00</span>
+                <p>24°C</p>
+            </div>
+            <img src="@/assets/sun.png" alt="sun" class="mx-auto" style="height: 75px;">
+        </div>
+        <div class="card bg-dark m-2" style="text-align: center;">
+            <div class="card-body">
+                <span class="text-white-50">24 Saat   </span><span>13:00</span>
+                <p>24°C</p>
+            </div>
+            <img src="@/assets/sun.png" alt="sun" class="mx-auto" style="height: 75px;">
+        </div>
+        <div class="card bg-dark m-2" style="text-align: center;">
+            <div class="card-body">
+                <span class="text-white-50">24 Saat   </span><span>13:00</span>
+                <p>24°C</p>
+            </div>
+            <img src="@/assets/sun.png" alt="sun" class="mx-auto" style="height: 75px;">
+        </div>
+        <div class="card bg-dark m-2" style="text-align: center;">
+            <div class="card-body">
+                <span class="text-white-50">24 Saat   </span><span>13:00</span>
+                <p>24°C</p>
+            </div>
+            <img src="@/assets/sun.png" alt="sun" class="mx-auto" style="height: 75px;">
+        </div>
     </div>
 </template>
 
@@ -17,10 +42,10 @@
 
 export default {
     props: {
-        currentData: Object, 
+        currentData: Object,
     },
     computed: {
-        currentTemperature() { 
+        currentTemperature() {
             return Math.round(this.currentData.main.temp)
         }
     }
